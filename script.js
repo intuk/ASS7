0e48cd67ed38f191275289f9206859d4a3cde9d9
intuk <ntuk_idong@gmail.com>
2024-05-07 01:07:32 -0500

    18th commit

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.mobile-menu')
+const xLogo = document.querySelector('.hamburger')
 
 hamburger.addEventListener('click', ()=> {
     nav.classList.toggle('visible')
 })
 
+hamburger.addEventListener('click', ()=> {
+    xLogo.classList.toggle('xlogo')
+})
